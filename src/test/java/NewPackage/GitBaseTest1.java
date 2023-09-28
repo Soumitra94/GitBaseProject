b1edc7d1f141bb1f2b0540c94fba6767dfa48399
Soumitra94 <soumitradhar53@gmail.com>
2023-09-28 15:42:28 +0530

    commit done

---
+++ src/test/java/NewPackage/GitBaseTest1.java
@@ -0,0 +1,14 @@
+package NewPackage;
+
+import org.testng.annotations.Test;
+
+public class GitBaseTest1 
+{
+	@Test
+	public void gitBase1()
+	{
+		System.out.println("-----Git Base 1 is Done-------");
+	}
+	
+
+}
