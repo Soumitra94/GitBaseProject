5011a80118b9ef6b6d412afabe22baf10c839a0e
Soumitra94 <soumitradhar53@gmail.com>
2023-09-28 13:40:44 +0530

    added

---
+++ src/test/java/NewPackage/GitBaseTest.java
@@ -0,0 +1,13 @@
+package NewPackage;
+
+import org.testng.annotations.Test;
+
+public class GitBaseTest 
+{
+	@Test
+	public void gitBase()
+	{
+		System.out.println("----He I am A Git Base Project----");
+	}
+
+}
